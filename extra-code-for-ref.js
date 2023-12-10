7828892561e30c89e3486230841abb274e695ce3
skynetpanda9 <akashpandya18@outlook.com>
2023-12-10 15:28:31 +0530

    connect mongo atlas

---
+++ extra-code-for-ref.js
@@ -0,0 +1,10 @@
+// db connection approach 1 in main index.js
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//   } catch (error) {
+//     console.error("ERROR", error);
+//     throw error;
+//   }
+// })();
